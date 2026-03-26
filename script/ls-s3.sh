3bf59eb4c51dd4edca417639c9763d310fa53e50
heiwa4126 <heiwa4126@gmail.com>
2026-03-26 11:31:05 +0900

    some cleanup

--- script/ls-s3.sh
+++ script/ls-s3.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # user1Stackで作ったS3バケットの中身をlsする。
+set -euo pipefail
+
 . ./.env
-BACKETNAME=$(jq .User1Stack.BucketName outputs1.tmp.json -r)
-aws s3 ls s3://$BACKETNAME --recursive --profile "$user1"
+BUCKET_NAME=$(jq .User1Stack.BucketName outputs1.tmp.json -r)
+aws s3 ls s3://"$BUCKET_NAME" --recursive --profile "$user1"
--- script/run-lambda.sh
+++ script/run-lambda.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # user1Stackで作ったlambda Function を実行する。user2StackのS3バケットに1個ファイルが増える(はず)。
+set -euo pipefail
+
 URL=$(jq .User2Stack.testFunctionUrl outputs2.tmp.json -r)
 curl "$URL" | jq .
